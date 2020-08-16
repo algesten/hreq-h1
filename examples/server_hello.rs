3bf724604f4ded4d939a0df5c6dcda30753eaf85
Martin Algesten <martin@algesten.se>
2020-08-16 10:25:30 +0200

    Clean up server_hello

--- examples/server_hello.rs
+++ examples/server_hello.rs
@@ -4,27 +4,26 @@ use hreq_h1::Error;
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
-    // use tracing::Level;
-    // use tracing_subscriber::FmtSubscriber;
-
-    // let sub = FmtSubscriber::builder()
-    //     .with_max_level(Level::TRACE)
-    //     .finish();
-
-    // tracing::subscriber::set_global_default(sub).expect("tracing set_global_default");
-
-    let l = TcpListener::bind("127.0.0.1:3000").await?;
+    let mut l = TcpListener::bind("127.0.0.1:3000").await?;
 
     println!("Listening to {:?}", l.local_addr().unwrap());
+    listen(&mut l).await;
 
+    Ok(())
+}
+
+// #[tracing::instrument(skip(l))]
+async fn listen(l: &mut TcpListener) {
     loop {
         let (tcp, _) = l.accept().await.expect("Accept incoming");
 
         let conn = hreq_h1::server::handshake(tcp);
 
-        async_std::task::spawn(async move {
+        let task = async move {
             handle_conn(conn).await.ok();
-        });
+        };
+
+        async_std::task::spawn(task);
     }
 }
 
@@ -38,9 +37,7 @@ async fn handle_conn(mut conn: Connection<TcpStream>) -> Result<(), Error> {
             .body(())
             .unwrap();
 
-        let send_body = respond.send_response(resp, false)?;
-
-        let mut send_body = send_body.ready().await?;
+        let mut send_body = respond.send_response(resp, false)?;
 
         send_body.send_data(b"Hello world!\n", true).await?;
     }
