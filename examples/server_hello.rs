c0cfc53bb8ff52fedd7bfbf655a4c022dde62775
Martin Algesten <martin@algesten.se>
2020-08-19 19:22:56 +0200

    Tweaks to server_hello example

--- examples/server_hello.rs
+++ examples/server_hello.rs
@@ -2,6 +2,8 @@ use async_std::net::{TcpListener, TcpStream};
 use hreq_h1::server::Connection;
 use hreq_h1::Error;
 
+use tracing_futures::Instrument;
+
 #[async_std::main]
 async fn main() -> Result<(), Error> {
     let mut l = TcpListener::bind("127.0.0.1:3000").await?;
@@ -14,14 +16,22 @@ async fn main() -> Result<(), Error> {
 
 // #[tracing::instrument(skip(l))]
 async fn listen(l: &mut TcpListener) {
+    let mut conn_no: u64 = 0;
     loop {
         let (tcp, _) = l.accept().await.expect("Accept incoming");
 
+        let port = tcp.peer_addr().unwrap().port();
         let conn = hreq_h1::server::handshake(tcp);
+
+        let no = conn_no;
+        conn_no += 1;
+
+        let span = tracing::info_span!("conn_task", port, no);
 
         let task = async move {
             handle_conn(conn).await.ok();
-        };
+        }
+        .instrument(span);
 
         async_std::task::spawn(task);
     }
@@ -29,7 +39,7 @@ async fn listen(l: &mut TcpListener) {
 
 async fn handle_conn(mut conn: Connection<TcpStream>) -> Result<(), Error> {
     while let Some(x) = conn.accept().await {
-        let (_, respond) = x.expect("Handshaken");
+        let (_, respond) = x?;
 
         let resp = http::Response::builder()
             .header("content-type", "text/plain")
@@ -37,7 +47,7 @@ async fn handle_conn(mut conn: Connection<TcpStream>) -> Result<(), Error> {
             .body(())
             .unwrap();
 
-        let mut send_body = respond.send_response(resp, false)?;
+        let mut send_body = respond.send_response(resp, false).await?;
 
         send_body.send_data(b"Hello world!\n", true).await?;
     }
