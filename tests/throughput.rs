457cb8bf34c7ba13cfe943048a7edec57b9f23f5
Martin Algesten <martin@algesten.se>
2020-08-22 09:25:58 +0200

    restructure throughput test

--- tests/throughput.rs
+++ tests/throughput.rs
@@ -15,31 +15,29 @@ async fn throughput_server_to_client() -> Result<(), hreq_h1::Error> {
     const PER_CHUNK: usize = 100 * 1024 * 1024; // 100 MB
     const CHUNKS: usize = 10 * 10; // 10GB in total
 
-    let span = tracing::info_span!("server_task");
+    let server = async move {
+        let (s, _) = l.accept().await.unwrap();
+        let mut s_conn = hreq_h1::server::handshake(s);
 
-    async_std::task::spawn(
-        async move {
-            let (s, _) = l.accept().await.unwrap();
-            let mut s_conn = hreq_h1::server::handshake(s);
+        let (_, send_res) = s_conn.accept().await.unwrap().unwrap();
 
-            let (_, send_res) = s_conn.accept().await.unwrap().unwrap();
+        let res = http::Response::builder()
+            .header("content-length", (PER_CHUNK * CHUNKS).to_string())
+            .body(())
+            .unwrap();
 
-            let res = http::Response::builder()
-                .header("content-length", (PER_CHUNK * CHUNKS).to_string())
-                .body(())
-                .unwrap();
+        let mut send_body = send_res.send_response(res, false).await.unwrap();
 
-            let mut send_body = send_res.send_response(res, false).await.unwrap();
+        for _ in 0..CHUNKS {
+            let chunk = vec![42_u8; PER_CHUNK];
 
-            for _ in 0..CHUNKS {
-                let chunk = vec![42_u8; PER_CHUNK];
-
-                send_body.send_data_owned(chunk, false).await.unwrap();
-            }
-            send_body.send_data(&[], true).await.unwrap();
+            send_body.send_data_owned(chunk, false).await.unwrap();
         }
-        .instrument(span),
-    );
+        send_body.send_data(&[], true).await.unwrap();
+    }
+    .instrument(tracing::info_span!("server_task"));
+
+    async_std::task::spawn(server);
 
     let c = c.await?;
     let (mut send_req, c_conn) = hreq_h1::client::handshake(c);
@@ -91,48 +89,46 @@ async fn throughput_client_to_server() -> Result<(), hreq_h1::Error> {
     const PER_CHUNK: usize = 100 * 1024 * 1024; // 100 MB
     const CHUNKS: usize = 10 * 10; // 10GB in total
 
-    let span = tracing::info_span!("server_task");
+    let server = async move {
+        let (s, _) = l.accept().await.unwrap();
+        let mut s_conn = hreq_h1::server::handshake(s);
 
-    async_std::task::spawn(
-        async move {
-            let (s, _) = l.accept().await.unwrap();
-            let mut s_conn = hreq_h1::server::handshake(s);
+        let (req, send_res) = s_conn.accept().await.unwrap().unwrap();
 
-            let (req, send_res) = s_conn.accept().await.unwrap().unwrap();
+        let (_, mut recv_body) = req.into_parts();
 
-            let (_, mut recv_body) = req.into_parts();
+        let mut into = vec![0_u8; PER_CHUNK];
+        let mut total = 0;
 
-            let mut into = vec![0_u8; PER_CHUNK];
-            let mut total = 0;
-
-            while total < PER_CHUNK * CHUNKS {
-                let mut pos = 0;
-                loop {
-                    let amt = recv_body.read(&mut into[pos..]).await.unwrap();
-                    pos += amt;
-                    total += amt;
-                    // println!(
-                    //     "{:.4} add {}",
-                    //     (total as f64 / (PER_CHUNK * CHUNKS) as f64),
-                    //     amt
-                    // );
-                    if pos == into.len() {
-                        pos = 0;
-                    }
-                    if amt == 0 {
-                        break;
-                    }
+        while total < PER_CHUNK * CHUNKS {
+            let mut pos = 0;
+            loop {
+                let amt = recv_body.read(&mut into[pos..]).await.unwrap();
+                pos += amt;
+                total += amt;
+                // println!(
+                //     "{:.4} add {}",
+                //     (total as f64 / (PER_CHUNK * CHUNKS) as f64),
+                //     amt
+                // );
+                if pos == into.len() {
+                    pos = 0;
+                }
+                if amt == 0 {
+                    break;
                 }
             }
-
-            let res = http::Response::builder().body(()).unwrap();
-
-            let mut send_body = send_res.send_response(res, false).await.unwrap();
-
-            send_body.send_data(&[], true).await.unwrap();
         }
-        .instrument(span),
-    );
+
+        let res = http::Response::builder().body(()).unwrap();
+
+        let mut send_body = send_res.send_response(res, false).await.unwrap();
+
+        send_body.send_data(&[], true).await.unwrap();
+    }
+    .instrument(tracing::info_span!("server_task"));
+
+    async_std::task::spawn(server);
 
     let c = c.await?;
     let (mut send_req, c_conn) = hreq_h1::client::handshake(c);
